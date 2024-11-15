0c419a8cc5e7210bc8f19a11dc4e93b34d480e82
Lucas-RodriguezAncieta <lucassantiago.lsra@gmail.com>
2024-11-14 20:05:41 -0400

    pagina principal front

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  { path: '', component: HomeComponent }, // Ruta principal (Home)
+  { path: 'login', component: LoginComponent }, // Ruta para el login
+  { path: '**', redirectTo: '' } // Redirige a Home si la ruta no existe
+];
