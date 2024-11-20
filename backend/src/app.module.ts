2d0184783edaeedfd1a69382675ef2a4018eb12a
Aldrin <axzt2004@gmail.com>
2024-11-19 20:16:58 -0400

    modulo evento-permiso

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,18 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsuarioModule } from './modules/usuario/usuario.module';
 
 @Module({
   imports: [
+    // Configuración de TypeORM
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',          // Cambia a la dirección de tu servidor de base de datos
       port: 3306,                 // Cambia el puerto si no es el predeterminado
-      username: 'root',     // Cambia a tu usuario de MySQL
-      password: '1234',  // Cambia a tu contraseña de MySQL
+      username: 'root',           // Cambia a tu usuario de MySQL
+      password: '130417140336',   // Cambia a tu contraseña de MySQL
       database: 'proyecto_sisinfo', // Cambia al nombre de tu base de datos
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,          // ¡Cuidado! Esto sincroniza automáticamente la base de datos en desarrollo, pero desactívalo en producción
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], // Busca entidades en todo el proyecto
+      synchronize: true,          // Sincronización automática (solo en desarrollo)
+      logging: ['query', 'error'], // Muestra las queries y errores en consola
     }),
+    UsuarioModule, // Importa el módulo de usuarios
   ],
 })
 export class AppModule {}
