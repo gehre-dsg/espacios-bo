133ae52b05b554bbb68bba71c9e7fa4f1caa4c42
Aldrin <axzt2004@gmail.com>
2024-11-25 10:53:33 -0400

    pre-pull

--- backend/src/modules/evento-permiso/evento-permiso.service.ts
+++ backend/src/modules/evento-permiso/evento-permiso.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { EventoPermiso } from '../../entities/evento_permiso.entity';
-import { Evento } from '../../entities/evento.entity';
-import { Permiso } from '../../entities/permiso.entity';
 
 @Injectable()
 export class EventoPermisoService {
@@ -12,55 +10,33 @@ export class EventoPermisoService {
     private readonly eventoPermisoRepository: Repository<EventoPermiso>,
   ) {}
 
-  // Crear un nuevo EventoPermiso
-  async create(
-    id_evento: number,
-    id_permiso: number,
-    documento: Buffer,
-  ): Promise<EventoPermiso> {
-    const eventoPermiso = this.eventoPermisoRepository.create({
-      id_evento,
-      id_permiso,
-      documento,
-    });
-    return this.eventoPermisoRepository.save(eventoPermiso);
-  }
-
-  // Obtener todos los registros
-  async findAll(): Promise<EventoPermiso[]> {
+  findAll(): Promise<EventoPermiso[]> {
     return this.eventoPermisoRepository.find({
-      relations: ['evento', 'permiso'],
+      relations: ['evento', 'permiso'], // Incluye las relaciones
     });
   }
 
-  // Obtener un registro específico
-  async findOne(id_evento: number, id_permiso: number): Promise<EventoPermiso> {
+  findOne(id_evento: number, id_permiso: number): Promise<EventoPermiso> {
     return this.eventoPermisoRepository.findOne({
       where: { id_evento, id_permiso },
       relations: ['evento', 'permiso'],
     });
   }
 
-  // Actualizar un registro
+  create(data: Partial<EventoPermiso>): Promise<EventoPermiso> {
+    const nuevoEventoPermiso = this.eventoPermisoRepository.create(data);
+    return this.eventoPermisoRepository.save(nuevoEventoPermiso);
+  }
+
   async update(
     id_evento: number,
     id_permiso: number,
-    documento: Buffer,
-  ): Promise<EventoPermiso> {
-    const eventoPermiso = await this.findOne(id_evento, id_permiso);
-    if (!eventoPermiso) {
-      throw new Error('EventoPermiso no encontrado');
-    }
-    eventoPermiso.documento = documento;
-    return this.eventoPermisoRepository.save(eventoPermiso);
+    data: Partial<EventoPermiso>,
+  ): Promise<void> {
+    await this.eventoPermisoRepository.update({ id_evento, id_permiso }, data);
   }
 
-  // Eliminar un registro
-  async remove(id_evento: number, id_permiso: number): Promise<void> {
-    const eventoPermiso = await this.findOne(id_evento, id_permiso);
-    if (!eventoPermiso) {
-      throw new Error('EventoPermiso no encontrado');
-    }
-    await this.eventoPermisoRepository.remove(eventoPermiso);
+  async delete(id_evento: number, id_permiso: number): Promise<void> {
+    await this.eventoPermisoRepository.delete({ id_evento, id_permiso });
   }
 }
